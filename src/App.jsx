a78155bed4bb0f5e4455c8480916902034dc1206
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-05-30 16:30:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/header"
+import Meme from "./components/memes"
+
+export default function App() {
+    return (
+        <div>
+           <Header />
+            <Meme />
+        </div>
+    )
+}
